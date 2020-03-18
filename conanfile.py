f8ddc02a766d5511967d4598326cfb062aa3e808
Spreequalle <ErikHoppe@web.de>
2020-03-18 18:31:01 +0100

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -7,12 +7,11 @@ class ZenGithubConan(ConanFile):
     license = "Apache 2.0"
     url = "https://github.com/jonico/libzengithub"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "no_asm": [True, False]}
-    default_options = "shared=True", "no_asm=True"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
     generators = "cmake"
     exports_sources = "zengithub/*"
     requires = "libcurl/7.64.1@bincrafters/stable"
-
     
     def configure(self):
         self.options["openssl"].shared = True
